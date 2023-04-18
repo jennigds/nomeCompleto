75a806cb2c66530e2b08b29a0efb61a0fdccf667
Jennigds <jennifergds89@gmail.com>
2023-04-17 21:50:42 -0300

    Resultado

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string nome, sobrenome;
+Console.WriteLine("Digite seu nome: ");
+nome = Console.ReadLine()!;
+
+Console.WriteLine("Digite seu sobrenome: ");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+
+Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
